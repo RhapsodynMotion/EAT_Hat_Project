e486762cba0d4a96b7d81863f99b084101f0a3c7
Steven <lam.steven94@yahoo.com>
2013-11-05 22:00:21 -0800

    Added methods to add,remove, and resize the member array

--- EAT_Hat_Project/src/MemberList.java
+++ EAT_Hat_Project/src/MemberList.java
@@ -8,9 +8,39 @@
  * @author Steven
  */
 public class MemberList {
-   
+
+   private Member[] memberList;
+   private int mCount;
+   private static final int DEFAULT_SIZE = 10;
+
    public MemberList() {
-      
+      memberList = new Member[DEFAULT_SIZE];
    }
-   
+
+   public void add(Member memberAdded) {
+      resizeIfNecessary();
+      memberList[mCount++] = memberAdded;
+   }
+
+   public void remove(String name) {
+      for (int i = 0; i < mCount; i++) {
+         if (memberList[i].getName() == name) {
+            shiftLeft(i);
+            mCount--;
+         }
+      }
+   }
+
+   private void resizeIfNecessary() {
+      if (mCount == memberList.length) {
+         Member[] newMemberList = new Member[memberList.length * 2];
+         System.arraycopy(memberList, 0, newMemberList, 0, mCount);
+         memberList = newMemberList;
+      }
+   }
+
+   private void shiftLeft(int startNdx) {
+      System.arraycopy(memberList, startNdx + 1, memberList, startNdx,
+       mCount - startNdx - 1);
+   }
 }
