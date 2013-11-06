dacfac973a8a9300e2beaaa4122191664923abc6
Steven <lam.steven94@yahoo.com>
2013-11-06 09:27:39 -0800

    Added a for loop to test if the program is adding members to the list

--- EAT_Hat_Project/src/Main.java
+++ EAT_Hat_Project/src/Main.java
@@ -17,12 +17,15 @@ public class Main {
       Scanner keyboard = new Scanner(System.in);
       String fileName;
       int memberCount = 0;
+      boolean accurateFile = true;
 
       MemberList memberList = new MemberList();
       
+      // While loop to keep keep checking until theres an accurate file name
+      // to load
+      while(accurateFile) {
       System.out.println("What is the member list file name?");
       fileName = keyboard.nextLine();
-      
       
       // Method to add member (names/multiplier) to a member list
       try {
@@ -34,13 +37,17 @@ public class Main {
             newMember = memberLoader.readNextMember();
          }
          System.out.println("Loaded " + memberCount
-          + " recipes! \n");
+          + " Members! \n");
+         accurateFile = false;
       }
       catch (IOException e) {
          System.out.println("Could not open the file "
-          + fileName);
+          + fileName + "\n");
+      }
       }
       
-      
+      for(int i = 0; i < memberList.getTotalMembers(); i++) {
+         System.out.println(memberList.getMember(i));
+      }
    }
 }
