3b245dcd29310557ce9373f2d9b22cf3d29a8679
Steven <lam.steven94@yahoo.com>
2013-11-05 21:37:13 -0800

    Added throw/catch to read in a file name

--- EAT_Hat_Project/src/Main.java
+++ EAT_Hat_Project/src/Main.java
@@ -3,13 +3,38 @@
  * and open the template in the editor.
  */
 
+import java.io.IOException;
+import java.util.Scanner;
+
 /**
  *
  * @author Steven
  */
 public class Main {
-   
-   public static void main(String [] args) {
-      
+
+   public static void main(String[] args) {
+
+      Scanner keyboard = new Scanner(System.in);
+      String fileName;
+      int memberCount = 0;
+
+      System.out.println("What is the member list file name?");
+      fileName = keyboard.nextLine();
+      // Method to add member names/multiplier to a member list
+      try {
+         MemberLoader memberLoader = new MemberLoader(fileName);
+         Member newMember = memberLoader.readNextMember();
+         while (newMember != null) {
+            MemberList.add(newMember);
+            memberCount++;
+            newMember = memberLoader.readNextMember();
+         }
+         System.out.println("Loaded " + memberCount
+          + " recipes! \n");
+      }
+      catch (IOException e) {
+         System.out.println("Could not open the file "
+          + fileName);
+      }
    }
 }
