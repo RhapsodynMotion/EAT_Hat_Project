927049fd2acd4eb64574cbf9a866cdb629724b3d
Steven <lam.steven94@yahoo.com>
2013-11-05 21:38:27 -0800

    Added a new class to hold all the members // Need to decide on data structure

---
+++ EAT_Hat_Project/src/MemberList.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Steven
+ */
+public class MemberList {
+   
+   public MemberList() {
+      
+   }
+   
+}
